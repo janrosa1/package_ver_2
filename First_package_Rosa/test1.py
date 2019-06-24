1072324bdb1f0dfb99c7411f33c5fffcd1114cd6
Jan_Rosa <jan.rosa1993@gmail.com>
2019-06-24 13:00:07 +0200

    pushing new requirements file

--- First_package_Rosa/test1.py
+++ First_package_Rosa/test1.py
@@ -8,11 +8,11 @@ Created on Sat Jun 22 19:41:21 2019
 import model_with_labour as l2
 import model_without_labour as l1
 
-test_1 = l1.life_cycle_1()
-test_1.policy_functions()
-test_1.execute_life_cycle()
-
-test_2 = l2.life_cycle_2()
-test_2.policy_functions()
-test_2.execute_life_cycle()
-
+# test_1 = l1.life_cycle_1()
+# test_1.policy_functions()
+# test_1.execute_life_cycle()
+#
+# test_2 = l2.life_cycle_2()
+# test_2.policy_functions()
+# test_2.execute_life_cycle()
+# 
